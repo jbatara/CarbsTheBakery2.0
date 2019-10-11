60e4ae7d21798d4e2154d309f006558dfd3b34a6
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-10-11 16:04:43 -0700

    Add routes for vendors, orders, and adding new vendors

--- Bakery/Controllers/VendorController.cs
+++ Bakery/Controllers/VendorController.cs
@@ -1,1 +1,29 @@
 
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Bakery.Models;
+
+
+namespace Bakery.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendor")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpGet("/vendor/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/vendor/new")]
+    public ActionResult AddNew(string name)
+    {
+      Vendor newVendor = new Vendor(name);
+      return Redirect("/vendor");
+    }
+  }
+}
